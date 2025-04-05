1ff2e79caeff6a0f66419326589d7b0718716b3d
ibrahem <hemaabdoo390@gmail.com>
2025-04-05 09:24:28 +0200

    send to login

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -20,5 +20,8 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         //
+        if (!app()->runningInConsole() && request()->is('/') && !request()->is('welcome')) {
+            redirect('/admin')->send();
+        }
     }
 }
